14f0254ef6ff8c8c2c44335206bbb256f007dcb5
ace-archer <ace-archer@users.noreply.github.com>
2021-01-16 13:00:10 -0600

    Update Configuration.h
    Hard code the default probe offset (for the mount im using)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1036,7 +1036,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -38, -8, 0}
+#define NOZZLE_TO_PROBE_OFFSET { -38, -8, -2.3}
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
